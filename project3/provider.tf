c8b571e2fd11c08f6e498a2f11351c18dba68d4c
sameergi <139549974+sameergi@users.noreply.github.com>
2023-11-10 12:23:51 +0530

    Update provider.tf

--- project3/provider.tf
+++ project3/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
- access_key = "AKIAZKEUSSMRB323VY43"
-  secret_key = "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"
+ access_key = "AKIAZKEUSSMRIO33KVWR"
+  secret_key = "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"
  region ="ap-south-1"
 }
